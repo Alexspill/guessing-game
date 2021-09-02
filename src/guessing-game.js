a509ba2942f5157fbda5c7f1af1f31adb78d3f3e
Alexander <almi78@mail.ru>
2021-09-03 00:18:49 +0300

    NewTask

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,21 @@
 class GuessingGame {
-    constructor() {}
-
+    
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.round((this.min + this.max)/2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
-
 module.exports = GuessingGame;
